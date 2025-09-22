correct quaternion in record script, table gets mapped in nvblox, correct scanning trajectories, added all camera data in recording (tensors and np)

motion_planning_trajectory_execution/scripts/load_camera_data.py
@@ -8,23 +8,24 @@ import torch
 import os
 import cv2
 
-from curobo.types.math import Pose
+# from curobo.types.math import Pose
 
 
-LOAD_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_recorded_data3.pt")
+LOAD_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_data_4.pt")
 
 # Load tensor file
 # torch.serialization.add_safe_globals([Pose])
-frames = torch.load(LOAD_PATH, weights_only=True)
+frames = torch.load(LOAD_PATH, weights_only=False)
 print(f"Loaded {len(frames)} frames")
 print("Available keys:", frames[0].keys())
 
 # Inspect all frames with OpenCV
 for i, frame in enumerate(frames):
-    depth = frame["depth"].cpu().numpy()             # [480, 640]
-    intrinsics = frame["intrinsics"].cpu().numpy()   # [3, 3]
-    position = frame["position"].cpu().numpy() 
-    quaternion = frame["quaternion"].cpu().numpy() 
+    depth = frame["depth_tensor"].cpu().numpy()             # [480, 640]
+    rgb = frame["rgb_np"]
+    intrinsics = frame["intrinsics_tensor"].cpu().numpy()   # [3, 3]
+    position = frame["position_tensor"].cpu().numpy() 
+    quaternion = frame["quaternion_tensor"].cpu().numpy() 
 
     # Simple depth colormap visualization
     depth_colormap = cv2.applyColorMap(
@@ -35,6 +36,7 @@ for i, frame in enumerate(frames):
     print(quaternion)
     # Display depth
     cv2.imshow("Depth", depth_colormap)
+    cv2.imshow("RGB", rgb)
     # print(f"Frame {i}: Intrinsics:\n{intrinsics}\nPose: {pose}")
 
     key = cv2.waitKey(100)  # 100 ms per frame

motion_planning_trajectory_execution/scripts/motion_planning_v2.py
@@ -89,8 +89,8 @@ from curobo.util.usd_helper import UsdHelper
 from curobo.util.logger import setup_curobo_logger
 
 
-LOAD_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_recorded_data3.pt")
-# LOAD_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_data_4.pt")
+# LOAD_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_recorded_data3.pt")
+LOAD_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_data_4.pt")
 
 # Load tensor file
 frames = torch.load(LOAD_PATH, weights_only=False)
@@ -107,7 +107,7 @@ if __name__ == "__main__":
 
     my_world = World(stage_units_in_meters=1.0)
     stage = my_world.stage
-    my_world.scene.add_default_ground_plane()
+    my_world.scene.add_default_ground_plane(z_position=-1.0)
 
     xform = stage.DefinePrim("/World", "Xform")
     stage.SetDefaultPrim(xform)
@@ -139,6 +139,8 @@ if __name__ == "__main__":
                     "pose": [0, 0, 0, 1, 0, 0, 0],
                     "integrator_type": "tsdf",
                     "voxel_size": 0.02,
+                    # "min_bound": [-5.0, -5.0, -1.0],
+                    # "max_bound": [5.0, 5.0, 5.0]
                 }
             }
         }
@@ -151,14 +153,14 @@ if __name__ == "__main__":
     j_names = robot_cfg["kinematics"]["cspace"]["joint_names"]
     default_config = robot_cfg["kinematics"]["cspace"]["retract_config"]
 
-    robot, _ = add_robot_to_scene(robot_cfg, my_world) #, position=np.array([0.25, 0, 0]))
+    robot, _ = add_robot_to_scene(robot_cfg, my_world) #, position=np.array([0, 0, 0.5]))
 
     articulation_controller = robot.get_articulation_controller()
-    world_cfg_table = WorldConfig.from_dict(
-        load_yaml(join_path(get_world_configs_path(), "collision_table.yml"))
-    )
-    world_cfg_table.cuboid[0].pose[2] -= 0.04
-    world_cfg.add_obstacle(world_cfg_table.cuboid[0])
+    # world_cfg_table = WorldConfig.from_dict(
+    #     load_yaml(join_path(get_world_configs_path(), "collision_table.yml"))
+    # )
+    # world_cfg_table.cuboid[0].pose[2] -= 0.04
+    # world_cfg.add_obstacle(world_cfg_table.cuboid[0])
     
     motion_gen_config = MotionGenConfig.load_from_robot_config(
         robot_cfg,
@@ -210,7 +212,7 @@ if __name__ == "__main__":
     mapping_done = False
 
     if not args.use_debug_draw:
-        voxel_viewer = VoxelManager(10000, size=render_voxel_size)
+        voxel_viewer = VoxelManager(5000, size=render_voxel_size)
 
     while simulation_app.is_running():
         my_world.step(render=True)
@@ -236,7 +238,7 @@ if __name__ == "__main__":
             )
 
         if step_index % 5 == 0.0 and mapping_done == False:
-            world_model.decay_layer("world")
+            # world_model.decay_layer("world")
             if frame_idx < len(frames):
                 current_frame = frames[frame_idx]
 
@@ -245,24 +247,33 @@ if __name__ == "__main__":
                 # position = tensor_args.to_device(current_frame["position"])
                 # quaternion = tensor_args.to_device(current_frame["quaternion"])
                 
-                print("position: ", current_frame["position"].cpu().numpy())
-                print("quaternion: ", current_frame["quaternion"].cpu().numpy())
+                print("position: ", current_frame["position_tensor"].cpu().numpy())
+                print("quaternion: ", current_frame["quaternion_tensor"].cpu().numpy())
 
-                quaternion_corrected = np.array([current_frame["quaternion"][3],current_frame["quaternion"][0], current_frame["quaternion"][1], current_frame["quaternion"][2]])
+                # quaternion_corrected = np.array([current_frame["quaternion"][3],current_frame["quaternion"][0], current_frame["quaternion"][1], current_frame["quaternion"][2]])
+
+                # new_position = current_frame["position_tensor"].cpu().numpy()
+                # new_position[2] += 0.5
+
+                camera_pose = Pose(
+                    position=tensor_args.to_device(current_frame["position_tensor"].cpu().numpy()),
+                    quaternion=tensor_args.to_device(current_frame["quaternion_tensor"].cpu().numpy()),
+                )
+
+                # camera_pose = Pose(
+                #     position=tensor_args.to_device(new_position),
+                #     quaternion=tensor_args.to_device(current_frame["quaternion_tensor"].cpu().numpy()),
+                # )
 
                 # camera_pose = Pose(
                 #     position=tensor_args.to_device(current_frame["position"].cpu().numpy()),
-                #     quaternion=tensor_args.to_device(current_frame["quaternion"].cpu().numpy()),
+                #     quaternion=tensor_args.to_device(quaternion_corrected),
                 # )
 
-                camera_pose = Pose(
-                    position=tensor_args.to_device(current_frame["position"].cpu().numpy()),
-                    quaternion=tensor_args.to_device(quaternion_corrected),
-                )
-
                 data_camera = CameraObservation(
-                    depth_image=current_frame["depth"],
-                    intrinsics=current_frame["intrinsics"],
+                    rgb_image=current_frame["rgba_tensor"],
+                    depth_image=current_frame["depth_tensor"],
+                    intrinsics=current_frame["intrinsics_tensor"],
                     pose=camera_pose
                 )
                 # data_camera = CameraObservation(
@@ -291,10 +302,20 @@ if __name__ == "__main__":
                 if voxels.shape[0] > 0:
                     # print("IM INSIDEEEEEEEEE")
                     # print(f"[DEBUG] Total voxels in world model: {voxels.shape[0]}")
-                    voxels = voxels[voxels[:, 2] > voxel_size]
+                    # voxels = voxels[voxels[:, 2] > voxel_size]
+                    voxels = voxels[voxels[:, 2] > -0.5]
+                    # voxels = voxels[voxels[:, 2] > -0.1]
                     voxels = voxels[voxels[:, 0] > 0.0]
+                    # voxels = voxels[voxels[:, 0] > 0.03 | voxels[:, 0] < -0.03]
+                    # voxels = voxels[(voxels[:, 0] > 0.03) | (voxels[:, 0] < -0.03)]
+                    # voxels = voxels[voxels[:, 1] > 0.03 | voxels[:, 1] < -0.03]
+                    # voxels = voxels[(voxels[:, 1] > 0.03) | (voxels[:, 1] < -0.03)]
+
+                    # voxels = voxels[voxels[:, 0] > 0.0]
+                    # voxel_viewer.update_voxels(voxels[:, :3].cpu().numpy())
                     # Count after filtering
                     # print(f"[DEBUG] Voxels after filtering: {voxels.shape[0]}")
+                    # voxels = voxels
 
                     if args.use_debug_draw:
                     #     draw_points(voxels)
@@ -315,29 +336,29 @@ if __name__ == "__main__":
                 mapping_done = True
                 
                 # Save map as .nvblox
-                world_model.save_layer("world", "nvblox_map_test.nvblx")
+                # world_model.save_layer("world", "nvblox_map_test.nvblx")
                 
-                # Save map as mesh
-                mesh = world_model.get_mesh_from_blox_layer("world", mode="nvblox")
-                print("Num vertices:", len(mesh.vertices))
-                print("Num faces:", len(mesh.faces))
-                print("Has colors:", mesh.vertex_colors is not None and len(mesh.vertex_colors) > 0)
+                # # Save map as mesh
+                # mesh = world_model.get_mesh_from_blox_layer("world", mode="nvblox")
+                # print("Num vertices:", len(mesh.vertices))
+                # print("Num faces:", len(mesh.faces))
+                # print("Has colors:", mesh.vertex_colors is not None and len(mesh.vertex_colors) > 0)
 
-                # Convert to numpy
-                vertices = np.array(mesh.vertices, dtype=np.float32)
-                faces = np.array(mesh.faces, dtype=np.int32)
+                # # Convert to numpy
+                # vertices = np.array(mesh.vertices, dtype=np.float32)
+                # faces = np.array(mesh.faces, dtype=np.int32)
 
-                # If colors exist, convert too
-                colors = None
-                if hasattr(mesh, "vertex_colors") and mesh.vertex_colors is not None:
-                    colors = np.array(mesh.vertex_colors, dtype=np.uint8)
+                # # If colors exist, convert too
+                # colors = None
+                # if hasattr(mesh, "vertex_colors") and mesh.vertex_colors is not None:
+                #     colors = np.array(mesh.vertex_colors, dtype=np.uint8)
 
-                # Build trimesh object
-                trimesh_mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_colors=colors)
+                # # Build trimesh object
+                # trimesh_mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_colors=colors)
 
-                # Export
-                trimesh_mesh.export("mesh_map_test.obj")
-                print(f"✅ Mesh saved")
+                # # Export
+                # trimesh_mesh.export("mesh_map_test2.obj")
+                # print(f"✅ Mesh saved")
 
 
                 # Mapper.save_map()

motion_planning_trajectory_execution/utils/test_camera_node.py
@@ -1,41 +1,67 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# MOTION STACK MUST BE RAN FIRST
 
 import rclpy
-from rclpy.node import Node
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
-from geometry_msgs.msg import TransformStamped
-
+import yaml
 import cv2
 import torch
 import os
 
-from nvblox_torch.datasets.realsense_dataset import RealsenseDataloader
-from curobo.types.math import Pose
+from rclpy.node import Node
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+from geometry_msgs.msg import TransformStamped
+from ament_index_python.packages import get_package_share_directory
 
 
-SAVE_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", "camera_recorded_data3.pt")  # file to save all frames
-NUM_FRAMES = 300  # how many frames to record
+from motion_planning_trajectory_execution.utils.realsense_dataset import RealsenseDataloader
+# from nvblox_torch.datasets.realsense_dataset import RealsenseDataloader
 
-class RealsenseMinimal(Node):
-    def __init__(self):
-        super().__init__("realsense_minimal")
+class CameraRecorder(Node):
+    def __init__(self, camera_recorder_config=None):
+        super().__init__("camera_recorder")
+
+        # Get package path
+        package_share_path = get_package_share_directory("motion_planning_trajectory_execution")
+        print("PKG DONE")
+
+        # Load YAML config file
+        if camera_recorder_config is None:
+            cfg_path = os.path.join(package_share_path, "configs", "camera_recorder_config.yaml")
+        else:
+            cfg_path = os.path.join(package_share_path, "configs", camera_recorder_config)
+
+        try:
+            with open(cfg_path, "r", encoding="utf-8") as f:
+                self.camera_recorder_cfg = yaml.safe_load(f)
+        except FileNotFoundError:
+            raise SystemExit("Camera recording config file not found!")
+        except yaml.YAMLError as e:
+            raise SystemExit(f"YAML syntax error: {e}")
+
+        # Get parameters from config file
+        self.frame_a = self.camera_recorder_cfg["frame_a"]
+        self.frame_b = self.camera_recorder_cfg["frame_b"]
+        self.save_rate = self.camera_recorder_cfg["save_rate"]
+        self.max_frames = self.camera_recorder_cfg["max_frames"]
+        clipping_distance = self.camera_recorder_cfg["clipping_distance"]
+        file_name = self.camera_recorder_cfg["save_file"]
+
+        # Start Realsense
+        self.realsense_data = RealsenseDataloader(clipping_distance_m=clipping_distance)
 
         # TF listener
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self.realsense_data = RealsenseDataloader(clipping_distance_m=0.5)
-
-        # Transform of <a> with respect to <b>
-        self.frame_a = "camera_color_optical_frame"
-        self.frame_b = "base_link"
-
-        # Frames 
-        self.frames = []        # List of frames data
-        self.save_rate = 5     # Save every n frames
+        # Initialize variables
+        self.frames = []        # List of frames' data
         self.frames_saved = 0
+        
+        # Path for saving frames data
+        # self.save_path = os.path.join(os.path.dirname(__file__), "..", "..", "camera_data", file_name)
+        self.save_path = os.path.join(package_share_path, "camera_data", file_name)
 
 
     def wait_for_tf(self):
@@ -61,6 +87,29 @@ class RealsenseMinimal(Node):
         key = cv2.waitKey(1)
         return key
 
+    # def visualize_color_depth(self, color_np, depth_np):
+    #     """Show depth and color frames using OpenCV"""
+
+    #     # Ensure color is uint8
+    #     if color_np.dtype != 'uint8':
+    #         color_disp = (color_np * 255).astype('uint8')
+    #     else:
+    #         color_disp = color_np.copy()
+
+    #     # Convert RGB → BGR for OpenCV
+    #     color_disp = cv2.cvtColor(color_disp, cv2.COLOR_RGB2BGR)
+
+    #     # Depth colormap
+    #     depth_colormap = cv2.applyColorMap(
+    #         cv2.convertScaleAbs(depth_np, alpha=100), cv2.COLORMAP_VIRIDIS
+    #     )
+
+    #     # Stack side by side
+    #     stacked = cv2.hconcat([color_disp, depth_colormap])
+    #     cv2.imshow("Camera Recording: Color + Depth", stacked)
+    #     key = cv2.waitKey(1)
+    #     return key
+
 
     def run(self):
         """Main run"""
@@ -76,40 +125,35 @@ class RealsenseMinimal(Node):
                 self.get_logger().info(
                     f"Got frame {frame_counter}: Depth {data['depth'].shape}, Intrinsics {data['intrinsics'].shape}"
                 )
-                depth = data["depth"]
+                depth_tensor = data["depth"]
                 depth_np = data["raw_depth"]
-                color_np = data["raw_rgb"]
-                intrinsics = data["intrinsics"]
+                rgb_np = data["raw_rgb"].copy()
+                rgba_tensor = data["rgba"]
+                rbga_nvblox_tensor = data["rgba_nvblox"]
+                intrinsics_tensor = data["intrinsics"]
 
                 # 3. Get latest camera --> base_link transform 
                 tf: TransformStamped = self.tf_buffer.lookup_transform(
                     self.frame_b, self.frame_a, rclpy.time.Time()
                 )
 
-                # print(tf.transform.translation)
-                # print(tf.transform.rotation)
-
                 # 4. Allocate camera pose
-                position = torch.tensor([tf.transform.translation.x,
-                                         tf.transform.translation.y,
-                                         tf.transform.translation.z],
-                                        dtype=torch.float32)
-                quaternion = torch.tensor([tf.transform.rotation.w,
-                                           tf.transform.rotation.x,
-                                           tf.transform.rotation.y,
-                                           tf.transform.rotation.z],
-                                          dtype=torch.float32)
+                position_tensor = torch.tensor([tf.transform.translation.x,
+                                                tf.transform.translation.y,
+                                                tf.transform.translation.z],
+                                                dtype=torch.float32)
+                quaternion_tensor = torch.tensor([tf.transform.rotation.w,
+                                                  tf.transform.rotation.x,
+                                                  tf.transform.rotation.y,
+                                                  tf.transform.rotation.z],
+                                                  dtype=torch.float32)
                 
-                # print(position.dtype)
-                # print(quaternion.dtype)
-                
-                # camera_pose = Pose(position=position, quaternion=quaternion)
                 print(f"Got camera position and quaternion for frame {frame_counter}")
 
                 frame_counter +=1
 
                 # 5. Visualize color and depth frames
-                key = self.visualize_color_depth(color_np, depth_np)
+                key = self.visualize_color_depth(rgb_np, depth_np)
                 if key & 0xFF == ord("q") or key == 27:  # q or ESC to close and stop recording
                     self.get_logger().info("Recording interrupted by user.")
                     break
@@ -120,31 +164,38 @@ class RealsenseMinimal(Node):
             rclpy.spin_once(self, timeout_sec=0.01)
             
             # Stop recording once the max number of frames has been saved
-            if self.frames_saved == NUM_FRAMES:
+            if self.frames_saved == self.max_frames:
                 break
             
             # 6. Save frames data in list
             if frame_counter % self.save_rate == 0:
                 self.frames.append({
-                    "depth": depth,
-                    "intrinsics": intrinsics,
-                    # "pose": camera_pose
-                    "position": position,
-                    "quaternion": quaternion 
+                    # "depth": depth,
+                    # "intrinsics": intrinsics,
+                    # "position": position,
+                    # "quaternion": quaternion 
+                    "depth_tensor": depth_tensor,
+                    "depth_np": depth_np,
+                    "rgb_np": rgb_np,
+                    "rgba_tensor": rgba_tensor,
+                    "rbga_nvblox_tensor": rbga_nvblox_tensor,
+                    "intrinsics_tensor": intrinsics_tensor,
+                    "position_tensor": position_tensor,
+                    "quaternion_tensor": quaternion_tensor
                 })
                 self.frames_saved += 1
-                self.get_logger().info(f"Recorded frame {frame_counter}, {self.frames_saved}/{NUM_FRAMES}")
+                self.get_logger().info(f"Recorded frame {frame_counter}, {self.frames_saved}/{self.max_frames}")
                 
         # 7. Export frames data list to file
-        torch.save(self.frames, SAVE_PATH)
-        self.get_logger().info(f"Saved {len(self.frames)} frames to {SAVE_PATH}")
+        torch.save(self.frames, self.save_path)
+        self.get_logger().info(f"Saved {len(self.frames)} frames to {self.save_path}")
 
         self.realsense_data.stop_device()
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = RealsenseMinimal()
+    node = CameraRecorder()
     node.run()
     node.destroy_node()
     rclpy.shutdown()